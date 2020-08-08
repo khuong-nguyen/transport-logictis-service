b1b17f1ae9fbee8c6ce25d013ed5e5a80615315f
Kha Nguyen <acquykoten@gmail.com>
2020-08-08 16:33:17 +0700

    Multi-Language route and dashboard

---
+++ resources/lang/en/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'dashboard' => 'Dashboard',
+
+    'booking_module' => 'Booking Module',
+    'request_order_registration' => 'Request Order Registration',
+    'request_order_inquiry' => 'Request Order Inquiry',
+    'virtual_booking_registration' => 'Virtual Booking Registration',
+    'virtual_booking_inquiry' => 'Virtual Booking Inquiry',
+    'booking_registration' => 'Booking Registration',
+    'booking_inquiry' => 'Booking Inquiry',
+
+    'transportation_module' => 'Transportation Module',
+    'inventory_module' => 'Inventory Module',
+    'setting_module' => 'Setting Module',
+
+];
---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,54 @@
+<div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
+    <div class="c-sidebar-brand d-lg-down-none">
+        <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+            <use xlink:href="assets/brand/coreui.svg#full"></use>
+        </svg>
+        <svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
+            <use xlink:href="assets/brand/coreui.svg#signet"></use>
+        </svg>
+    </div>
+    <ul class="c-sidebar-nav">
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="/">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-speedometer"></use>
+                </svg>@lang('sidebar.dashboard')
+            </a>
+        </li>
+        <li class="c-sidebar-nav-title">@lang('sidebar.booking_module')</li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-book"></use>
+                </svg>@lang('sidebar.request_order_registration')</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-book"></use>
+                </svg>@lang('sidebar.request_order_inquiry')</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-book"></use>
+                </svg>@lang('sidebar.virtual_booking_registration')</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-book"></use>
+                </svg>@lang('sidebar.virtual_booking_inquiry')</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-book"></use>
+                </svg>@lang('sidebar.booking_registration')</a></li>
+        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="assets/icons/sprites/free.svg#cil-book"></use>
+                </svg>@lang('sidebar.booking_inquiry')</a></li>
+
+        <li class="c-sidebar-nav-divider"></li>
+        <li class="c-sidebar-nav-title">@lang('sidebar.transportation_module')</li>
+
+        <li class="c-sidebar-nav-divider"></li>
+        <li class="c-sidebar-nav-title">@lang('sidebar.inventory_module')</li>
+
+        <li class="c-sidebar-nav-divider"></li>
+        <li class="c-sidebar-nav-title">@lang('sidebar.setting_module')</li>
+    </ul>
+    <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
+</div>
